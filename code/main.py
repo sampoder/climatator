45cbdf679f8bbb06b7e9003cc23d72dba416592d
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-03 05:55:05 +0100

    Add Lighting

--- code/main.py
+++ code/main.py
@@ -1,14 +1,71 @@
 import asyncio
-from kasa import SmartPlug
+from smartplug import SmartPlug
+import time
+import board
+import neopixel
+from rpi_ws281x import *
 
-async def main():
+pixel_pin = board.D18
+
+num_pixels = 144
+
+ORDER = neopixel.RGB
+pixels= neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+ 
+
+async def turnOffFan():
     p = SmartPlug("192.168.1.144")
 
     await p.update()
-    print(p.is_on)
 
     await p.turn_off()
 
+async def turnOnFan():
+    p = SmartPlug("192.168.1.144")
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    await p.update()
+
+    await p.turn_on()
+
+def colorWipe(strip, color, color2, color3, wait_ms=50):
+    for i in range(num_pixels):
+	    strip.fill(color)
+    while True:
+        for i in range(num_pixels):
+            strip[i] = color2
+            strip.show()
+            time.sleep(wait_ms/10000.0)
+        for i in range(num_pixels):
+            strip[i] = color3
+            strip.show()
+            time.sleep(wait_ms/10000.0)
+        for i in range(num_pixels):
+            strip[i] = color
+            strip.show()
+            time.sleep(wait_ms/10000.0)
+    strip.clear()
+    
+colorWipe(pixels, Color(40, 0, 0),  Color(40, 5, 0),  Color(40, 10, 0))
---
+++ code/smartplug.py
@@ -0,0 +1,70 @@
+"""Module for smart plugs (HS100, HS110, ..)."""
+import logging
+from typing import Any, Dict
+
+from smartdevice import DeviceType, SmartDevice, requires_update
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SmartPlug(SmartDevice):
+    """Representation of a TP-Link Smart Switch.
+    To initialize, you have to await :func:`update()` at least once.
+    This will allow accessing the properties using the exposed properties.
+    All changes to the device are done using awaitable methods,
+    which will not change the cached values, but you must await :func:`update()` separately.
+    Errors reported by the device are raised as :class:`SmartDeviceException`s,
+    and should be handled by the user of the library.
+    Examples:
+        >>> import asyncio
+        >>> plug = SmartPlug("127.0.0.1")
+        >>> asyncio.run(plug.update())
+        >>> plug.alias
+        Kitchen
+        Setting the LED state:
+        >>> asyncio.run(plug.set_led(True))
+        >>> asyncio.run(plug.update())
+        >>> plug.led
+        True
+    For more examples, see the :class:`SmartDevice` class.
+    """
+
+    def __init__(self, host: str) -> None:
+        super().__init__(host)
+        self.emeter_type = "emeter"
+        self._device_type = DeviceType.Plug
+
+    @property  # type: ignore
+    @requires_update
+    def is_on(self) -> bool:
+        """Return whether device is on."""
+        sys_info = self.sys_info
+        return bool(sys_info["relay_state"])
+
+    async def turn_on(self, **kwargs):
+        """Turn the switch on."""
+        return await self._query_helper("system", "set_relay_state", {"state": 1})
+
+    async def turn_off(self, **kwargs):
+        """Turn the switch off."""
+        return await self._query_helper("system", "set_relay_state", {"state": 0})
+
+    @property  # type: ignore
+    @requires_update
+    def led(self) -> bool:
+        """Return the state of the led."""
+        sys_info = self.sys_info
+        return bool(1 - sys_info["led_off"])
+
+    async def set_led(self, state: bool):
+        """Set the state of the led (night mode)."""
+        return await self._query_helper(
+            "system", "set_led_off", {"off": int(not state)}
+        )
+
+    @property  # type: ignore
+    @requires_update
+    def state_information(self) -> Dict[str, Any]:
+        """Return switch-specific state information."""
+        info = {"LED state": self.led, "On since": self.on_since}
+        return info
